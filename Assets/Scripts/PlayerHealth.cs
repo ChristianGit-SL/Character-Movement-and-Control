211e38424434db5e8f926e55a4cff33afde2b10a
ChristianGit-SL <christiancuahuey@gmail.com>
2026-05-07 12:48:36 -0700

    Try GameScene to Start Scene

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using DG.Tweening;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour {
 
     private float maxHealth = 100f;
     private float currentHealth;
     public GameObject deathText;
+
+    [SerializeField] private string mainMenuSceneName = "Start Menu";
+    [SerializeField] private float returnDelay = 2f;
 
     // private float amount = currentHealth / maxHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -32,7 +36,15 @@ public class PlayerHealth : MonoBehaviour {
     public void OnPlayerDeath() {
         deathText.SetActive(true);
 
+        Invoke(nameof(ReturnToMainMenu), returnDelay);
+
+        // gameObject.SetActive(false);
+
         
+    }
+
+    private void ReturnToMainMenu() {
+        SceneManager.LoadScene(mainMenuSceneName);
     }
 
 
